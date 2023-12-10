55dc27f0f2d07773e4c498a86f1555ee7e909e69
ActuaryEmma <emmanyakio@gmail.com>
2023-12-10 16:16:34 +0300

    CRUD operations

--- backend/server.py
+++ backend/server.py
@@ -1,26 +1,78 @@
-# from flask import Flask, render_template
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
-# app = Flask(__name__, static_url_path="/", static_folder="./client/build")
-# app.config.from_object(config)
-# @app.route("/home")
-# def home():
-#     #return ("Hello ExploreKe")
-#     return app.send_static_file("index.html")
+app = Flask(__name__)
+CORS(app)
 
-# if __name__=="__main__":
-#     app.run(debug=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://nyakio:password@localhost/ExploreKe'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text())
+    postDate = db.Column(db.DateTime, default = datetime.datetime.now)
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+class ArticleSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'title', 'content', 'postDate')
 
 
-from flask import Flask
 
-app = Flask(__name__, static_url_path="/", static_folder="./client/build")
-# Assuming you have a config object defined somewhere
-# app.config.from_object(config)
+article_schema = ArticleSchema()
+articles_schema = ArticleSchema(many=True)
 
-@app.route("/home")
-def home():
-    # return ("Hello ExploreKe")
-    return app.send_static_file("index.html")
 
-if __name__ == "__main__":
+@app.route('/get', methods = ['GET'])
+def get_articles():
+    all_articles = Article.query.all()
+    results = articles_schema.dump(all_articles)
+    return jsonify(results)
+
+@app.route('/get/<id>/', methods = ['GET'])
+def get_article(id):
+    article = Article.query.get(id)
+    return article_schema.jsonify(article)
+
+@app.route('/add', methods = ['POST'])
+def add_article():
+    title = request.json['title']
+    content = request.json['body']
+    articles = Article(title, content)
+    db.session.add(articles)
+    db.session.commit()
+    return article_schema.jsonify(articles)
+
+@app.route('/update/<id>', methods = ['PUT'])
+def update_articles(id):
+    article = Article.query.get(id)
+    title = request.json['title']
+    content = request.json['content']
+
+    article.title = title
+    article.content = content
+
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+@app.route('/delete/<id>', methods = ['DELETE'])
+def delete_article(id):
+    article = Article.query.get(id)
+    db.session.delete(article)
+    db.session.commit()
+    return article_schema.jsonify(article)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
