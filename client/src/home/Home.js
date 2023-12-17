c0f60e2688b2f462d5fd2fc4ef444f5f1316b9a8
Brian Omondi <omondiibrian00@gmail.com>
2023-12-17 23:09:56 +0300

    Landing page mod

--- client/src/home/Home.js
+++ client/src/home/Home.js
@@ -3,8 +3,9 @@ import "./home.css"
 
 function Home(){
     return(
-        <div className="homepage">
-            <h1 class="text-[40px] text-white">UNLOCKING THE HEART OF ADVENTURE</h1>
+        <div className="homepage flex flex-col">
+            <h3 class="text-5xl text-[40px] text-white">UNLOCKING THE HEART OF ADVENTURE</h3>
+            <h3 class="text-5xl font-bold text-green-800">ONE TALE AT A TIME</h3>
         </div>
     )
 }
